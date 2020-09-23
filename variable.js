86d48f4df3e29d7926c3e25da25197a9fdd7f770
jayroncena <jayroncena@yahoo.com>
2020-09-23 22:57:53 +0900

    Update variable.js
    Practice declaring variables and assigning value to it.
    
    Practice contacatenation of string.

---
+++ variable.js
@@ -0,0 +1,9 @@
+var firstName = "Jayron";
+var lastName = "Cena";
+var age = 26;
+var birthdate = "1995-08-15";
+var wish = '"What is your wish?"';
+
+var welcomeMessage = "Hi " + firstName + "" + lastName + " ! Happy " + age + "th birtday!" + wish;
+
+console.log(welcomeMessage);
