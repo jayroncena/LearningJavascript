8508b719fffeda630a074d5ad5e6fe32e7e51c44
jayroncena <jayroncena@yahoo.com>
2020-09-25 21:23:39 +0900

    Added multipleCondition javascript
    Practicing multiple conditional statements inside if and else if

---
+++ multipleCondition.js
@@ -0,0 +1,28 @@
+var input = 0;
+
+if (1 === 1 && 2=== 2){
+    console.log("These are both true");
+}
+
+if (true && true){
+    console.log("These are the same!");
+}
+
+if ( 1===3 || "joe" === "joe"){
+    console.log("One of the conditions is true!");
+}
+
+
+var cat1 = 5;
+var cat2 = 10;
+var cat3 = 1;
+var disabled
+
+
+if(cat1 > cat2 && cat1 > cat3){
+    conole.log("Cat 1 is the cutest!");
+}else if (cat2 > cat1 && cat2 > cat3){
+    console.log("Cat 2 is the cutest!");
+}else if (cat3 > cat1 && cat3 > cat2){
+    console.log("Cat 3 is the cutest");
+}
