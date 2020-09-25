f738930a6ea3d9648be6d51a40a23103fc109ef8
jayroncena <jayroncena@yahoo.com>
2020-09-25 21:00:04 +0900

    Learn if and else if
    Learn if and else if

--- numbersAndOperators.js
+++ numbersAndOperators.js
@@ -12,4 +12,21 @@ var msg = "10 / 3 = 3 with a remainder of " + mod;
 
 var result = 10 * (5 + 3) - 4;
 
+
+var myAccountBalance = 500;
+var purchase = "500";
+
+if (myAccountBalance < purchase){
+    console.log("You are broke buddy!");
+}
+else if(myAccountBalance > purchase){
+    console.log("You can buy the item!");
+}
+else if(myAccountBalance === purchase){
+    console.log("Your money is exact!")
+}
+else if(myAccountBalance == purchase){
+    console.log("The value is the same but the type may or may not be the same");
+}
+
 console.log(result);
