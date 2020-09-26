237e988a84fa32d2ecbb7138868f440f1459d5bf
jayroncena <jayroncena@yahoo.com>
2020-09-26 14:14:46 +0900

    Update Loops.js

--- Loops.js
+++ Loops.js
@@ -1,3 +1,12 @@
-for (var x=0; x < 5; x++ ){
-    console.log("What are you doing " + x);
+/*var total = 10;
+
+for (var x=0; x < total; x++){
+    //iterate until not to
+    console.log(x);
+}*/
+
+var students = ["John","Jacob","Jingle","Heimer","Smith"];
+
+for (var x=0; x < students.length ; x++){
+    console.log("Student " + x + " is " + students[x]);
 }
