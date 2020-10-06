1b64d4947a3ccf5982c41de5c861319dcfc2c464
jayroncena <jayroncena@yahoo.com>
2020-10-06 21:25:58 +0900

    Update functions.js

--- functions.js
+++ functions.js
@@ -10,3 +10,18 @@ function getArea(length,width){
     
     return length*width;
 }
+
+var bankBalance = 500;
+
+function makeTransaction(priceSale) {
+    if (priceSale <= bankBalance){
+        bankBalance-= priceSale;
+        console.log("Puchase Successful");
+    } else{
+        console.log("Insufficient Fund");
+    }
+}
+
+makeTransaction(79.00);
+makeTransaction(2.32);
+makeTransaction(10.45);
